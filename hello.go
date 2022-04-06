b686023e3702063cce48595e5c7ca751f16b68f6
kjuiop <copying_y@naver.com>
2022-04-06 23:15:01 +0900

    push ready

--- hello.go
+++ hello.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world !")
+
+	var a int = 10
+	var msg string = "Hello Variable"
+
+	a = 20
+	msg = "Good Morning"
+	fmt.Println(msg, a)
+
+	var minimumWage int = 10
+	var workingHour int = 20
+
+	var income int = minimumWage * workingHour
+
+	fmt.Println(minimumWage, workingHour, income)
 }
