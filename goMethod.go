4cc423032b6fe43de90366da92ce861f3290d13b
kjuiop <copying_y@naver.com>
2022-04-18 20:21:16 +0900

    go method

--- goMethod.go
+++ goMethod.go
@@ -15,6 +15,41 @@ func methodCalculate() {
 	fmt.Println(myInt(b).add(50))
 }
 
+type account struct {
+	balance   int
+	firstName string
+	lastName  string
+}
+
+func (a1 *account) withdrawPointer(amount int) {
+	a1.balance -= amount
+}
+
+func (a2 account) withdrawValue(amount int) {
+	a2.balance -= amount
+}
+
+func (a3 account) withdrawReturnValue(amount int) account {
+	a3.balance -= amount
+	return a3
+}
+
+func pointerMethod() {
+	var mainA *account = &account{100, "Joe", "Park"}
+	mainA.withdrawPointer(30)
+	fmt.Println(mainA.balance)
+
+	mainA.withdrawValue(20)
+	fmt.Println(mainA.balance)
+
+	var mainB account = mainA.withdrawReturnValue(20)
+	fmt.Println(mainB.balance)
+
+	mainB.withdrawPointer(30)
+	fmt.Println(mainB.balance)
+}
+
 func methodEx() {
-	methodCalculate()
+	//methodCalculate()
+	pointerMethod()
 }
