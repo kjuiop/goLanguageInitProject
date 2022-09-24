54978a1172afaf4d7397f2f7af0d1f07b426dd25
kjuiop <copying_y@naver.com>
2022-09-24 10:59:13 +0900

    push ready

--- goCalcurate.go
+++ goCalcurate.go
@@ -13,6 +13,7 @@ func cal() {
 	fmt.Println("x - y = ", x-y)
 	fmt.Println("x * y = ", x*y)
 	fmt.Println("x * y = ", x*y)
+	fmt.Println("x * y = ", x*y)
 	fmt.Println("x / y = ", x/y)
 	fmt.Println("x % y = ", x%y)
 	fmt.Println("s * t = ", s*t)
